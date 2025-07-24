991e05b36bdb0b7d65faa994b5fe5ffdf81bef23
andres8073562 <espineldavid56.com>
2025-07-24 09:21:54 -0500

    feat: :fire: portafolio redesing

---
+++ Scripts/funciones.js
@@ -0,0 +1,71 @@
+
+
+function mostrarCuadro() {
+    const cuadro = document.getElementById("cuadro1");
+    if (cuadro.style.display === "none" || cuadro.style.display === "") {
+      cuadro.style.display = "flex";
+    } else {
+      cuadro.style.display = "none";
+    }
+  }
+
+ function ocultarCuadro() {
+    document.getElementById("cuadro1").style.display = "none";
+
+}
+
+function mostrarCuadro1() {
+    const cuadro = document.getElementById("cuadro2");
+    if (cuadro.style.display === "none" || cuadro.style.display === "") {
+      cuadro.style.display = "flex";
+    } else {
+      cuadro.style.display = "none";
+    }
+  }
+
+ function ocultarCuadro() {
+    document.getElementById("cuadro2").style.display = "none";
+
+}
+
+function mostrarCuadro2() {
+    const cuadro = document.getElementById("cuadro3");
+    if (cuadro.style.display === "none" || cuadro.style.display === "") {
+      cuadro.style.display = "flex";
+    } else {
+      cuadro.style.display = "none";
+    }
+  }
+
+ function ocultarCuadro() {
+    document.getElementById("cuadro3").style.display = "none";
+
+}
+
+function mostrarCuadro3() {
+    const cuadro = document.getElementById("cuadro4");
+    if (cuadro.style.display === "none" || cuadro.style.display === "") {
+      cuadro.style.display = "flex";
+    } else {
+      cuadro.style.display = "none";
+    }
+  }
+
+ function ocultarCuadro() {
+    document.getElementById("cuadro4").style.display = "none";
+
+}
+
+function mostrarCuadro4() {
+    const cuadro = document.getElementById("cuadro5");
+    if (cuadro.style.display === "none" || cuadro.style.display === "") {
+      cuadro.style.display = "flex";
+    } else {
+      cuadro.style.display = "none";
+    }
+  }
+
+ function ocultarCuadro() {
+    document.getElementById("cuadro5").style.display = "none";
+
+}
